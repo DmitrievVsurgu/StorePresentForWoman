e5edd69348899fcdec2a82766e6e34e69b9d3a43
Dmitriev Vladimir <Dmitriev.V.G11@yandex.ru>
2017-11-25 12:17:34 +0500

    Добавьте файлы проекта.

---
+++ StorePresentForWoman/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorePresentForWoman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
